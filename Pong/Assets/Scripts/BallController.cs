75742e76deb1ca36794b295b5a5be1a6715ed9b0
Fauzizain12 <48669935+Fauzizain12@users.noreply.github.com>
2022-06-09 20:35:46 +0700

    Adding UI & Game Manager
    Adding UI & Game Manager

--- Pong/Assets/Scripts/BallController.cs
+++ Pong/Assets/Scripts/BallController.cs
@@ -7,6 +7,7 @@ public class BallController : MonoBehaviour
 
     public Vector2 speed;
     private Rigidbody2D rig;
+    public Vector2 resetPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -14,5 +15,9 @@ public class BallController : MonoBehaviour
         rig = GetComponent<Rigidbody2D>();
         rig.velocity = speed;
     }
-  
+
+    public void ResetBall()
+    {
+        transform.position = new Vector3(resetPosition.x, resetPosition.y, 2);
+    }
 }
